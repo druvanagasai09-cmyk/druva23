474b1bf4213a703c9cb5c41101b2c327e4976151
druvanagasai09-cmyk <druvanagasai09@gmail.com>
2026-09-16 09:04:21 +0530

    Add files via upload

---
+++ d2.py
@@ -0,0 +1,6 @@
+total_classes = int(input("Enter total classes conducted: "))
+classes_attended = int(input("Enter classes attended: "))
+
+attendance_percentage = (classes_attended / total_classes) * 100
+
+print("Attendance percentage:", attendance_percentage, "%")
