e4355c99415ea510208005f20bf3fd751537ec38
druvanagasai09-cmyk <druvanagasai09@gmail.com>
2026-09-16 09:51:28 +0530

    Add files via upload

---
+++ d7.py
@@ -0,0 +1,14 @@
+marks = float(input("Enter marks obtained (0-100): "))
+
+if marks >= 90:
+    performance = "Excellent"
+elif marks >= 75:
+    performance = "Very Good"
+elif marks >= 60:
+    performance = "Good"
+elif marks >= 40:
+    performance = "Average"
+else:
+    performance = "Poor"
+
+print("Student's performance:", performance)
